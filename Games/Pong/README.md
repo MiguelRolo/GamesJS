6fbd00d082382507adae041e76b1045879e0e582
Rolex <manrrolo@gmail.com>
2023-09-29 16:33:10 +0100

    Pong game added

---
+++ Games/Pong/README.md
@@ -0,0 +1,57 @@
+# GamesJS 🎮
+
+- This open source repository contains a **collection of games** built on basic tech stacks in web development. 
+- Use your creativity, build your own game and contribute to the repository by making a PR
+- Make sure to ⭐️ the repository to stay tuned! 
+
+# Pong
+
+![Game Started](./assets/game-started.png)
+
+## Tech Stack ⚡
+
+![HTML5](https://img.shields.io/badge/HTML5-E34F26.svg?style=for-the-badge&logo=HTML5&logoColor=white)
+![CSS3](https://img.shields.io/badge/CSS3-1572B6.svg?style=for-the-badge&logo=CSS3&logoColor=white)
+![Javascript](https://img.shields.io/badge/Javascript-F7DF1E.svg?style=for-the-badge&logo=javascript&logoColor=black)
+
+## Description 📃
+> This game is developed by HTML, CSS and JS.
+>
+> Pong is a table tennis–themed twitch arcade sports video game, featuring simple two-dimensional graphics, manufactured by Atari and originally released in 1972.
+
+## How to play? 🕹️
+
+As you start the game, the ball starts moving in a random dyagonal direction, and then it works like a tennis games.
+
+To win, each Player/Paddle needs to defend their own field and also needs to make a point in their opponent field. 
+
+Once the Player/Paddle have reached the maximum score, the game is over.
+
+Keys:
+- *`W`* - **Move Left Paddle Up**
+- *`S`* - **Move Left Paddle Down**
+- ⬆️ *(Up Arrow Key)*- **Move Right Paddle Up**
+- ⬇️ *(Down Arrow Key)* - **Move Right Paddle Down**
+- `+` - **Increase Maximum Score**
+- `-` - **Decrease Maximum Score**
+- *`P`* - **Pause Game**
+- *`N`* - **New Game**
+
+## Screenshots 📸
+
+|                                         |                                            |
+|               :---:                     |                 :---:                      |
+| ![Game Start](./assets/game-start.png)  | ![Game Started](./assets/game-started.png) |
+| ![Game Paused](./assets/game-pause.png) | ![Game Over](./assets/game-over.png)       |
+
+## Further Exploration 🔭
+
+This are further exploration to add in the future, but feel free to implement it and/or give suggestion! 
+
+> *Nothing to add... for now 😎*
+
+## License 🪪
+
+> (Apache License, Version 2.0) You're free to use this game and code in any project, personal or commercial. 
+>
+> There's no need to ask permission before using these. Giving attribution is not required, but appreciated.
---
+++ Games/Pong/index.js
@@ -0,0 +1,285 @@
+const canvas = document.getElementById('jogo');
+const context = canvas.getContext('2d');
+
+canvas.style.border = '3px solid rgba(255, 255, 255, 0.2)';
+const grid = 15;
+
+const getRandomDirection = () => Math.random() < 0.5 ? -1 : 1;
+
+const paddleVelocity = 6;
+const paddleHeight = grid * 5;
+const paddleMaxY = canvas.height - grid - paddleHeight;
+const paddleLeft = {
+  x: grid * 2,
+  y: canvas.height / 2 - paddleHeight / 2,
+  width: grid,
+  height: paddleHeight,
+  dy: 0
+}
+
+const paddleRight = {
+  x: canvas.width - (grid * 3),
+  y: canvas.height / 2 - paddleHeight / 2,
+  width: grid,
+  height: paddleHeight,
+  dy: 0
+}
+
+const ballVelocity = 5 * getRandomDirection();
+const ball = {
+  x: canvas.width / 2 - grid / 2,
+  y: canvas.height / 2 - grid / 2,
+  width: grid,
+  height: grid,
+  reset: false,
+  dx: ballVelocity,
+  dy: -ballVelocity,
+}
+
+function Collides(obj1, obj2) {
+  return obj1.x < obj2.x + obj2.width &&
+    obj1.x + obj1.width > obj2.x &&
+    obj1.y < obj2.y + obj2.height &&
+    obj1.y + obj1.height > obj2.y;
+}
+
+function showGameStart() {
+  context.fillStyle = 'rgba(15,15,15,0.5)';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  context.fillStyle = 'firebrick';
+  context.fillRect(0, canvas.height / 2 - 30, canvas.width, 90);
+
+  context.globalAlpha = 1;
+  context.fillStyle = 'white';
+  context.font = '15px monospace';
+  context.textAlign = 'center';
+  context.textBaseline = 'middle';
+  context.fillText('START GAME', canvas.width / 2, canvas.height / 2);
+  context.fillText('Press "N" to start', canvas.width / 2, (canvas.height / 2) + 30);
+}
+
+function showGamePause() {
+  context.fillStyle = 'rgba(15,15,15,0.5)';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  context.globalAlpha = 1;
+  context.fillStyle = 'white';
+  context.font = '15px monospace';
+  context.textAlign = 'center';
+  context.textBaseline = 'middle';
+  context.fillText('GAME PAUSED', canvas.width / 2, canvas.height / 2);
+  context.fillText('Press "P" to continue', canvas.width / 2, (canvas.height / 2) + 30);
+}
+
+function showGameOver(paddle) {
+  cancelAnimationFrame(rAF);
+  gameOver = true;
+  gameStart = false;
+  
+
+  context.fillStyle = 'rgba(15,15,15,0.5)';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  context.fillStyle = 'firebrick';
+  context.fillRect(0, canvas.height / 2 - 60, canvas.width, 120);
+
+  context.globalAlpha = 1;
+  context.fillStyle = 'white';
+  context.font = '15px monospace';
+  context.textAlign = 'center';
+  context.textBaseline = 'middle';
+  context.fillText('GAME OVER', canvas.width / 2, canvas.height / 2 - 30);
+  context.fillText(
+    paddle === 'left' ? 'LEFT PADDLE WON!' : 
+    paddle === 'right' && 'RIGHT PADDLE WON!', 
+    canvas.width / 2, canvas.height / 2);
+  context.fillText('Press "N" to try again', canvas.width / 2, (canvas.height / 2) + 30);
+}
+
+function fpsMeter() {
+  let prevTime = Date.now();
+  let frames = 0;
+  let counter = document.getElementById('fps-counter');
+  
+  requestAnimationFrame(function meter() {
+    const time = Date.now();
+    frames++;
+    if (time > prevTime + 1000) {
+      let fps = Math.round( ( frames * 1000 ) / ( time - prevTime ) );
+      prevTime = time;
+      frames = 0;
+      
+      counter.innerText = ('0' + fps).slice(-2);
+    }
+    document.getElementById('max-score').innerText = ('0' + maxScore).slice(-2);
+    
+    requestAnimationFrame(meter);
+  })
+}
+
+let scoreLeft = 0;
+let scoreRight = 0;
+let maxScore = 5;
+let rAF = null;
+let gameOver = false;
+let gameStart = false;
+let gamePause = false;
+
+
+function game() {
+  if (!gamePause) {
+    rAF = requestAnimationFrame(game);
+  }
+
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
+  context.fillStyle = 'lightgrey';
+  context.fillRect(0,0,canvas.width, grid);
+  context.fillRect(0, canvas.height - grid, canvas.width, canvas.width);
+
+  context.font = '30px "Courier New", Courier, monospace';
+  context.fillText(('0' + scoreLeft).slice(-2), grid * 5, grid * 5);
+  context.fillText(('0' + scoreRight).slice(-2), canvas.width - (grid * 8), grid * 5);
+  
+  for (let i = grid; i < canvas.height - grid; i+= grid * 2) {
+    context.fillRect(canvas.width / 2 - grid / 2, i, grid, grid);
+  }
+
+  if (gameStart) {
+    paddleLeft.y += paddleLeft.dy;
+    paddleRight.y += paddleRight.dy;
+  
+    if (paddleLeft.y < grid) {
+      paddleLeft.y = grid;
+    } else if (paddleLeft.y > paddleMaxY) {
+      paddleLeft.y = paddleMaxY;
+    }
+  
+    if (paddleRight.y < grid) {
+      paddleRight.y = grid;
+    } else if (paddleRight.y > paddleMaxY) {
+      paddleRight.y = paddleMaxY;
+    }
+  
+    context.fillStyle = 'white';
+    context.fillRect(paddleLeft.x, paddleLeft.y, paddleLeft.width, paddleLeft.height);
+    context.fillRect(paddleRight.x, paddleRight.y, paddleRight.width, paddleRight.height);
+    
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+  
+    if (ball.y < grid) {
+      ball.y = grid;
+      ball.dy *= -1;
+    } else if (ball.y + grid > canvas.height - grid) {
+      ball.y = canvas.height - (grid * 2);
+      ball.dy *= -1;
+    }
+  
+    if (Collides(ball, paddleRight)) {
+      ball.dx *= -1;
+      ball.x = paddleRight.x - ball.width;
+    } else if (Collides(ball, paddleLeft)) {
+      ball.dx *= -1;
+      ball.x = paddleLeft.x + paddleLeft.width;
+    }
+  
+    if ((ball.x < 0 || ball.x > canvas.width) && !ball.reset) {
+      ball.reset = true;
+      
+      if (ball.x < 0) {
+        scoreRight++;
+        if (scoreRight === maxScore) {
+          setTimeout(() => showGameOver('right'), 250);
+        }
+      } else if (ball.x > canvas.width) {
+        scoreLeft++;
+        if (scoreLeft === maxScore) {
+          setTimeout(() => showGameOver('left'), 250);
+        }
+      }
+  
+      setTimeout(() => {
+        ball.reset = false;
+        ball.x = canvas.width / 2 - grid / 2;
+        ball.y = canvas.height / 2 - grid / 2;
+        ball.dx *= -1;
+        ball.dy *= getRandomDirection();
+      }, 500);
+    }
+  
+    context.fillStyle = 'white';
+    context.fillRect(ball.x, ball.y, ball.width, ball.height);
+    if (gamePause) {
+      showGamePause();
+    }
+  } else {
+    return showGameStart();
+  }
+}
+
+document.addEventListener('keydown',(e) => {
+  if (e.key === 'n') {
+    if (gameOver) {
+      rAF = requestAnimationFrame(game);
+      gameOver = false;
+    }
+    gameStart = true;
+    scoreLeft = 0;
+    scoreRight = 0;
+    ball.x = canvas.width / 2 - grid / 2;
+    ball.y = canvas.height / 2 - grid / 2;
+    ball.dx *= getRandomDirection();
+    ball.dy *= getRandomDirection();
+    paddleLeft.y = canvas.height / 2 - paddleHeight / 2;
+    paddleRight.y = canvas.height / 2 - paddleHeight / 2;
+  }
+
+  if (!gameStart || gameOver) {
+    if (e.key === '+' && maxScore < 99) {
+      maxScore++;
+    }
+    if (e.key === '-' && maxScore > 1) {
+      maxScore--;
+    }
+  }
+
+  if (gameStart) {
+    if (e.key === 'p') {
+      if (!gamePause) {
+        gamePause = true;
+      } else {
+        requestAnimationFrame(game);
+        gamePause = false;
+      }
+    }
+
+    if (e.key === 'ArrowUp') {
+      paddleRight.dy = -paddleVelocity;
+    }
+    if (e.key === 'ArrowDown') {
+      paddleRight.dy = paddleVelocity;
+    }
+    if (e.key === 'w') {
+      paddleLeft.dy = -paddleVelocity;
+    }
+    if (e.key === 's') {
+      paddleLeft.dy = paddleVelocity;
+    }
+  }
+})
+
+document.addEventListener('keyup', (e) => {
+  if (e.key === 'ArrowUp' || e.key === 'ArrowDown') {
+    paddleRight.dy = 0;
+  }
+  if (e.key === 'w' || e.key === 's') {
+    paddleLeft.dy = 0;
+  }
+})
+
+rAF = requestAnimationFrame(game);
+window.onload = () => {
+  fpsMeter();
+}
